b5cca98566325aeddb6e3a8da3ef30977ea7c611
9yuhyeon <kkh2742@gmail.com>
2022-10-27 00:03:58 +0900

    Customize TokenObtainPairSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from .models import User
 
@@ -20,3 +21,12 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+
+        return token
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
-
+from .serializers import CustomTokenObtainPairSerializer
 
 class UserView(APIView):
     def post(self, request):
@@ -11,3 +12,8 @@ class UserView(APIView):
             serializer.save()
             return Response({"msg":"가입 완료!!"}, status=status.HTTP_201_CREATED)
         return Response({"msg":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class CutomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
