b16de7d0aa763eba4b5245b10936b8042b686a7f
9yuhyeon <kkh2742@gmail.com>
2022-10-26 23:18:56 +0900

    add signup, login function in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"가입 완료!!"}, status=status.HTTP_201_CREATED)
+        return Response({"msg":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
